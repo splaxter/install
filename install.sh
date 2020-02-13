1cf5c3aebed94dc01f865eeba7640f3f64be31a8
Timo Klinge <36901551+splaxter@users.noreply.github.com>
2020-02-13 20:12:10 +0100

    kde

--- install.sh
+++ install.sh
@@ -2,3 +2,6 @@ sudo apt update -y && apt upgrade -y
 sudo apt install zsh wget git dnsmasq -y
 sudo sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+# KDE als standard
+$ sudo systemctl set-default graphical.target
