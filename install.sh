038b27a237c0b8812660344ce43ab4114e85c3e5
Timo Klinge <36901551+splaxter@users.noreply.github.com>
2020-02-13 20:39:15 +0100

    auto-suggest

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 #sudo apt update  && apt upgrade
 #sudo apt install zsh wget git dnsmasq 
-sudo sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-
+#sudo sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+#git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
+echo "source ${(q-)PWD}/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
 # KDE als standard
 #$ sudo systemctl set-default graphical.target
