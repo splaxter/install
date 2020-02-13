3922c11906ef086f7c158ef93753275ee16973d6
Timo Klinge <36901551+splaxter@users.noreply.github.com>
2020-02-13 19:56:06 +0100

    dnsmasq

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 sudo apt update -y && apt upgrade -y
-sudo apt install zsh wget git -y
+sudo apt install zsh wget git dnsmasq -y
 sudo sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
