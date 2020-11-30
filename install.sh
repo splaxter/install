5496201db84a57c584c525804d18de4c820cadd6
Timo Klinge <36901551+splaxter@users.noreply.github.com>
2020-11-30 12:57:08 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,11 +6,14 @@ cd temp
 wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh
 chmod +x install.sh
 ./install.sh -y
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+cd ~/.oh-my-zsh/plugins/
+git clone https://github.com/zsh-users/zsh-autosuggestions 
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
-sudo apt install powerline fonts-powerline -y
-sudo apt-get install zsh-theme-powerlevel9k -y
+wget https://github.com/powerline/fonts/blob/master/Hack/Hack-Regular.ttf
+cd 
+#sudo apt install powerline fonts-powerline -y
+#sudo apt-get install zsh-theme-powerlevel9k -y
 # write to .zshrc
-echo "source /usr/share/powerlevel9k/powerlevel9k.zsh-theme" >> ~/.zshrc
-sudo apt-get install zsh-syntax-highlighting
+#echo "source /usr/share/powerlevel9k/powerlevel9k.zsh-theme" >> ~/.zshrc
+#sudo apt-get install zsh-syntax-highlighting
 echo "source /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ~/.zshrc
