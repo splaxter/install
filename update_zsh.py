668737a76f22f88c95c4d7c89f331f0983810de5
Timo Klinge <36901551+splaxter@users.noreply.github.com>
2020-11-30 16:39:01 +0100

    Create update_zsh.py

---
+++ update_zsh.py
@@ -0,0 +1,17 @@
+from os.path import expanduser
+home = expanduser("~")
+
+
+theme = 'ZSH_THEME="agnoster"'
+plugins = "plugins=(git zsh-autosuggestions zsh-syntax-highlighting)"
+out = ""
+with open(f"{home}.zshrc" ,"r") as file:
+    for line in file:
+        if line.startswith("plugins"):
+            out += plugins
+        elif line.startswith("ZSH_THEME"):
+            out += theme
+        else:
+            out += line
+with open(f"{home}.zshrc", "w") as file:
+    file.write(out)
