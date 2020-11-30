c9eb8261a40016c455af1fb6107a2d6a0ca33e14
Timo Klinge <36901551+splaxter@users.noreply.github.com>
2020-11-30 13:52:53 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,18 +3,16 @@ sudo apt update  -y && apt upgrade -y
 sudo apt install zsh wget git -y
 mkdir temp
 cd temp
-wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh
-chmod +x install.sh
-mkdir -p ~/.oh-my-zsh/plugins/
-cd ~/.oh-my-zsh/plugins/
 git clone https://github.com/zsh-users/zsh-autosuggestions 
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
 wget https://github.com/powerline/fonts/blob/master/Hack/Hack-Regular.ttf
-cd -
+wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh
+chmod +x install.sh
 ./install.sh -y
+mv zsh-autosuggestions/ zsh-syntax-highlighting/ ~/.oh-my-zsh
 #sudo apt install powerline fonts-powerline -y
 #sudo apt-get install zsh-theme-powerlevel9k -y
 # write to .zshrc
 #echo "source /usr/share/powerlevel9k/powerlevel9k.zsh-theme" >> ~/.zshrc
 #sudo apt-get install zsh-syntax-highlighting
-echo "source /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ~/.zshrc
+#echo "source /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ~/.zshrc
